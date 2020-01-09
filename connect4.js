ca0bba706d949fd39590e2b0638b70646aa0b8f2
George Gruner <imgeorge220@gmail.com>
2020-01-09 11:55:14 -0800

    makeBoard arrays

--- connect4.js
+++ connect4.js
@@ -5,18 +5,24 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+const BOARD = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  let rowArray = [];
+  for (let i  = 0; i < WIDTH; i++){
+    rowArray.push(null);
+  }
+  for (let i = 0; i < HEIGHT; i++){
+    BOARD.push(rowArray);
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
@@ -109,7 +115,7 @@ function checkForWin() {
         y < HEIGHT &&
         x >= 0 &&
         x < WIDTH &&
-        board[y][x] === currPlayer
+        BOARD[y][x] === currPlayer
     );
   }
 
